1f330888c41ff1f45f28473b0a416aa2e963c927
hwangtate <ghkdxodud10253504@gmail.com>
2024-08-27 13:24:30 +0900

    Feat: chapter_04 과제 업로드

--- chapter_02/todo/urls.py
+++ chapter_02/todo/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.todo_list),
-    path("<int:todo_id>/", views.todo_detail),
+    path("", views.todo_list, name="todo_list"),
+    path("<int:todo_id>/", views.todo_detail, name="todo_detail"),
+    path("create/", views.todo_create, name="todo_create"),
+    path("<int:todo_id>/update/", views.todo_update, name="todo_update"),
+    path("<int:todo_id>/delete/", views.todo_delete, name="todo_delete"),
 ]
